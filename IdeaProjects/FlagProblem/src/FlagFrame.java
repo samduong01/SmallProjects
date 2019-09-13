23d4e84600078e7f135cd066fc63b5359113835a
Sam Duong <samduong@Sams-MacBook-Pro.local>
2019-09-13 11:06:04 -0700

    update

--- IdeaProjects/FlagProblem/src/FlagFrame.java
+++ IdeaProjects/FlagProblem/src/FlagFrame.java
@@ -8,23 +8,23 @@ import javax.swing.JFrame;
 
 public class FlagFrame extends JFrame {
     //scale field
-    float scale;
+    double scale;
 
     //window title bar height offset
     private final int offset = 23;
 
     //proportion fields
-    private long a;
-    private long b;
-    private long d;
+    private int a;
+    private int b;
+    private int d;
 
-    private long k;
-    private long l;
-    private long c;
-    private long e;
-    private long f;;
-    private long g;
-    private long h;
+    private int k;
+    private int l;
+    private int c;
+    private int e;
+    private int f;
+    private int g;
+    private int h;
 
     //coordinate
     private int[] originCords = new int[2];
@@ -32,22 +32,22 @@ public class FlagFrame extends JFrame {
     //sets proportion fields
     public FlagFrame(){
         this.scale = (float)600;
-        a = Math.round(1 * scale)-l/2;
-        b = Math.round(1.9 * scale);
-        d = Math.round(0.76 * scale);
+        a = (int)(1 * scale)-l/2;
+        b = (int)(1.9 * scale);
+        d = (int)(0.76 * scale);
 
-        k = Math.round(0.0616 * scale);
+        k = (int)(0.0616 * scale);
         l = (int)(a/13.0);
         c = 7*l;
-        e = (long)(c/10.0);
-        f = (long)(c/10.0);;
-        g = (long)(d/12.0);
-        h = (long)(d/12.0);
+        e = (int)(c/10.0);
+        f = (int)(c/10.0);
+        g = (int)(d/12.0);
+        h = (int)(d/12.0);
         init();
     }
 
     private void init() {
-        setSize((int)b,(int)a);
+        setSize(b,a);
         //set background to white
         setBackground(Color.WHITE);
         repaint();
@@ -56,25 +56,12 @@ public class FlagFrame extends JFrame {
     public void paint(Graphics g1) {
         //create Graphics2D object for drawing stars
         Graphics2D gr = (Graphics2D) g1;
-        //clear the window of previous drawings
-        g1.clearRect( 0 , 0 , getWidth() , getHeight() );
-        gr.clearRect(0,0,getWidth(),getHeight());
 
-        //set the scale to the window height
-        this.scale = (float)this.getHeight()-offset;
+        //update scale
+        updateScale();
 
-        //update proportions again
-        a = Math.round(1 * scale);
-        b = Math.round(1.9 * scale);
-        d = Math.round(0.76 * scale);
-
-        k = Math.round(0.0616 * scale);
-        l = (int)(a/13.0);
-        c = 7*l;
-        e = (long)(c/10.0);
-        f = (long)(c/10.0);
-        g = (long)(d/12.0);
-        h = (long)(d/12.0);
+        //clear window
+        clearWindow(g1,gr);
 
         //draw methods
         drawStripes(g1);
@@ -83,30 +70,54 @@ public class FlagFrame extends JFrame {
         drawBounds(g1);
     }
 
+    private void clearWindow(Graphics g, Graphics2D gr){
+        //clear the window of previous drawings
+        g.clearRect( 0 , 0 , getWidth() , getHeight() );
+        gr.clearRect(0,0,getWidth(),getHeight());
+    }
+
+    private void updateScale(){
+        //set the scale to the window height
+        this.scale = this.getHeight()-offset;
+
+        //update proportions again
+        a = (int)(1 * scale);
+        b = (int)(1.9 * scale);
+        d = (int)(0.76 * scale);
+
+        k = (int)(0.0616 * scale);
+        l = (int)(a/13.0);
+        c = 7*l;
+        e = (int)(c/10.0);
+        f = (int)(c/10.0);
+        g = (int)(d/12.0);
+        h = (int)(d/12.0);
+    }
+
     private void drawStripes(Graphics g){
         int y = offset;
         int x = 0;
         //draw stripes in constant intervals
         for(int i = 0;i<7;i++){
             g.setColor(Color.RED);
-            g.fillRect(x,y,(int)b,(int)l);
+            g.fillRect(x,y,b,l);
             if(i==0){
                 originCords[0] = x;
                 originCords[1] = y;
             }
             //update position of the next stripe
-            y += (int)l*2;
+            y += l*2;
         }
     }
 
     private void drawBounds(Graphics g){
         //draw the bounding box
         g.setColor(Color.BLACK);
-        g.drawRect(originCords[0], originCords[1],(int)b,(int)(l*13.0));
+        g.drawRect(originCords[0], originCords[1],b,(int)(l*13.0));
     }
 
     private void drawStars(Graphics2D gr){
-        long incrementY = originCords[1]+e;
+        int incrementY = originCords[1]+e;
         gr.setColor(Color.WHITE);
 
         //draw 9 rows of stars
@@ -114,13 +125,13 @@ public class FlagFrame extends JFrame {
             //if the row number is even draw 6 stars
             //else draw 5 stars
             if(j%2 == 0) {
-                long x = g+ originCords[0];
+                int x = g+ originCords[0];
                 for (int i = 0; i < 6; i++) {
                     gr.fill(createStar(x, incrementY, (k/5.0), k/2.0));
                     x += 2 * h;
                 }
             }else{
-                long x = g+h+ originCords[0];
+                int x = g+h+ originCords[0];
                 for(int i = 0;i<5;i++){
                     gr.fill(createStar(x, incrementY, (k/5.0), k/2.0));
                     x+=2*h;
@@ -134,7 +145,7 @@ public class FlagFrame extends JFrame {
     private void drawBlueBox(Graphics g){
         //draw blue box
         g.setColor(Color.BLUE);
-        g.fillRect(originCords[0], originCords[1],(int)d,(int)c);
+        g.fillRect(originCords[0], originCords[1],d,c);
     }
 
     private static Shape createStar(double centerX, double centerY, double innerRadius, double outerRadius){
